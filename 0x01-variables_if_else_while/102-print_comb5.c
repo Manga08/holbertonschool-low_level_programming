7ce597f1e24fc83180682a118fa0479527c6217f
Manga08 <bijuu5445@gmail.com>
2020-02-07 03:49:54 +0000

    [Featured]: Upload the file 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if ((a == 0 && b != 0) || (b > a))
+			{
+				putchar(a / 10 + '0');
+				putchar(a % 10 + '0');
+				putchar(' ');
+				putchar(b / 10 + '2');
+				putchar(b % 10 + '0');
+
+				if (a < 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
