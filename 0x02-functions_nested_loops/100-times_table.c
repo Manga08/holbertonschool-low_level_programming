34bde6aff814e26bfcc82675b33d8609b4a26fdf
Manga08 <bijuu5445@gmail.com>
2020-02-12 04:17:44 +0000

    [Fix]: the 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,42 +9,40 @@ void print_times_table(int n)
 {
 	int c, ch, k;
 
-	if (n >= 0 && n < 15)
+	if (n > 0 && n < 15)
 	{
 		for (c = 0; c <= n; c++)
 		{
-			for (ch = 0; ch <= n; ch++)
+			_putchar('0');
+			for (ch = 1; ch <= n; ch++)
 			{
-			if (ch != 0)
-			{
-				k = i * j;
-				_putchar (',');
-				_putchar (' ');
-				else if (k >= 10 && k < 100)
+				k = c * ch;
+				if (k <= 9)
 				{
-					_putchar (' ');
-					_putchar (k / 10 + '0');
-					_putchar (k % 10 + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
 				}
-				else if (k < 10)
+				else if (k > 9 && k <= 99)
 				{
-					_putchar (' ');
-					_putchar (' ');
-					_putchar (k + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
 				}
 				else
 				{
-					_putchar (k / 100 + '0');
-					_putchar ((k / 10) % 10 + '0');
-					_putchar (k % 10 + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(k / 100 + '0');
+					_putchar(k / 10 % 10 + '0');
+					_putchar(k % 10 + '0');
 				}
 			}
-			else
-			{
-				_putchar (k  + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar(10);
-	}
 	}
 }
