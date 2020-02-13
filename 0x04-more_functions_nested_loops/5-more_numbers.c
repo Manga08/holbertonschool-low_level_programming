f396a8aead4aea62d959a9d66245bf8ad59c7b28
Manga08 <bijuu5445@gmail.com>
2020-02-13 19:36:54 +0000

    [Fix]: The 0x04-more_functions_nested_loops directories files

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,11 @@ void more_numbers(void)
 	{
 		for (ch = 0; ch <= 14; ch++)
 		{
-			if (ch < 10)
-			{
-				_putchar(ch + '0');
-			}
-			else
+			if (ch > 9)
 			{
 				_putchar((ch / 10) + '0');
-				_putchar((ch % 10) + '0');
 			}
+				_putchar((ch % 10) + '0');
 		}
 		_putchar ('\n');
 	}
