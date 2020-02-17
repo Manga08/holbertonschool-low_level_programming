3fc6e63478b406627c39e03a12f349499c85b819
Manga08 <bijuu5445@gmail.com>
2020-02-17 21:55:51 +0000

    [Featured]: Upload the 0x05-pointers_arrays_strings directories files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - check the code for Holberton School students.
+ *@str: the variable
+ *
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; *(str + c) != '\0'; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
