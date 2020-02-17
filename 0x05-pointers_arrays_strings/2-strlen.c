5c2fa1efc56499e9c5cfa74d1c4423813ac9bed0
Manga08 <bijuu5445@gmail.com>
2020-02-17 21:14:19 +0000

    [Featured]: Upload the 0x05-pointers_arrays_strings directories files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_strlen - tell me the lenght
+ *@s: give me the word
+ *Return: gave c
+ */
+int _strlen(char *s)
+{
+	int c;
+
+
+	for (c = 0; *(s + c) != '\0'; c++)
+	{
+	}
+	return (c);
+}
