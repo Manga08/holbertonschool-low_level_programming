6927af06da6afc9fd0b01ab97772a3e779c600a0
Manga08 <bijuu5445@gmail.com>
2020-02-18 00:20:28 +0000

    [Featured]: Upload the 0x05-pointers_arrays_strings directories files

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 /**
- * print_rev - check the code for Holberton School students.
+ * rev_string - check the code for Holberton School students.
  *@s: the variable
- *
+ *Return: always 0
  */
 void rev_string(char *s)
 {
 	int c;
-        int i = 0;
-        int f = 0;
-        for (c = 0; *(s + c) != '\0' ; c++)
-        {
-        }
-        c = c - 1;
+	int i = 0;
+	int f = 0;
 
-        for (i = 0 ; i  <= c; i++)
-        {
-                f = s[i];
-                s[i] = s[c];
-                s[c] = f;
-                c--;
-        }
+	for (c = 0; *(s + c) != '\0' ; c++)
+	{
+	}
+	c = c - 1;
+	for (i = 0 ; i  <= c; i++)
+	{
+		f = s[i];
+		s[i] = s[c];
+		s[c] = f;
+		c--;
+	}
 }
