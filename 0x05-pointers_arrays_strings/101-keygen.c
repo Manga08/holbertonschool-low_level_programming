dffc10476f1337fc0af099dfff724cb154a0e637
Manga08 <bijuu5445@gamail.com>
2020-02-19 23:34:05 -0500

    [Featured]: Upload the 101-keygen.c file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int c, ch, r;
+	char array[50] = "";
+
+	srand(time(NULL));
+
+	r = 0;
+	for (c = 0; r < (2772 - 100); c++)
+	{
+		ch = 33 + rand() % 94;
+		array[c] = ch;
+		r += array[c];
+	}
+	array[c] = 2772 - r;
+	c++;
+	array[c] = '\0';
+
+	printf("%s", array);
+
+	return (0);
+}
