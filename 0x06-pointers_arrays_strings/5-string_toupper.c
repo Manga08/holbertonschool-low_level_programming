2145388d18f9f994d55d6de0b91384d62eeabbd9
Manga08 <bijuu5445@gmail.com>
2020-02-20 18:16:21 +0000

    [Update] The 0x06 directories files

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,19 @@
 #include "holberton.h"
 /**
  * string_toupper - check the code for Holberton School students.
- *
+ *@n: the pinter
  *Return: always 0
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
+	int c = 0;
+
+	for(c = 0; n[c]; c++)
+	{
+		if(n[c]>='a' && n[c]<='z')
+		{
+			n[c] -= 32;
+		}
+	}
+	return(n);
 }
