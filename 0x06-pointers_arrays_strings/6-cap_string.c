ffd88bb1e581a933669f39a2a2ca0693864e0557
Manga08 <bijuu5445@gamail.com>
2020-02-20 19:45:32 -0500

    [Update]: The 0x06 directories files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,12 @@ char *cap_string(char *n)
 {
 	int c = 0;
 
-	for (c = 0; n[c] != '\n'; c++)
+	if (s[c] >= 'a' && s[c] <= 'z')
 	{
-		if (n[c] >= 'a' && n[c] <= 'z')
-		{
+		s[c] -= 32;
+	}
+	for (c = 0; n[c] != '\0'; c++)
+	{
 			if (n[c - 1] == '\n' ||
 			   n[c - 1] == '\t' ||
 			   n[c - 1] == ' ' ||
