96d502b0cfcfb46319dc89056d357a9679f90dda
Manga08 <bijuu5445@gamail.com>
2020-02-20 23:19:19 -0500

    [Update]: The 0x06 directories files

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * infinite_add - check the code for Holberton School students.
+ * @n1: first variable.
+ * @n2: second variable.
+ * @r: a variable.
+ * @size_r: size.
+ * Return: 0.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_buffer - check the code for Holberton School students.
+ *@b: the variable
+ *@size: the size
+ *Return: always 0
+ */
+void print_buffer(char *b, int size)
+{
+}
