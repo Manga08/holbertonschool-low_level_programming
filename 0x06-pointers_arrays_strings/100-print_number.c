f4d401dc3ea1e3d17a288d5fe7911ad297aa92bb
Manga08 <bijuu5445@gamail.com>
2020-02-20 23:07:08 -0500

    [Update]: The 0x06 directories files

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_number - check the code for Holberton School students.
+ *@n: the variable
+ *Return: always 0
+ */
+void print_number(int n)
+{
+	unsigned int c;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		c = n - 1;
+	}
+	else
+		c = n;
+
+	if (c / 10)
+		print_number(c / 10);
+
+	_putchar(c % 10 + '0');
+}
