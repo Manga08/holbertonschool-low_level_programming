275c6f28b6dc6fa35ea9230f8edf05f82f9c1552
Manga08 <60020101+Manga08@users.noreply.github.com>
2020-02-24 13:54:41 -0500

    [Fix]: The 7-print_chessboard.c file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,14 +4,16 @@
  *@a: something
  *Return: always 0
  */
- void print_chessboard(char (*a)[8])
- {
+void print_chessboard(char (*a)[8])
+{
 	unsigned int c, ch;
 
 	for (c = 0; c < 8; c++)
 	{
 		for (ch = 0; ch < 8; ch++)
+		{
 		_putchar(a[c][ch]);
+		}
 	}
 	putchar('/n');
 }
