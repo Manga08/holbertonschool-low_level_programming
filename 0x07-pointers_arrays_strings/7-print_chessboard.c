609d3ce3065ee8e138b1db71399dee6ba012a570
Manga08 <60020101+Manga08@users.noreply.github.com>
2020-02-24 13:49:04 -0500

    [Featured]: Upload the 7-print_chessboard.c file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_chessboard - check the code for Holberton School students.
+ *@a: something
+ *Return: always 0
+ */
+ void print_chessboard(char (*a)[8])
+ {
+	unsigned int c, ch;
+
+	for (c = 0; s < 8; c++)
+	{
+		for (ch = 0; ch < 8; ch++)
+		_putchar(a[c][ch]);
+	}
+	putchar('/n');
+}
