4e11bb35589afad774ba1f01395de34ef4923aa3
Manga08 <60020101+Manga08@users.noreply.github.com>
2020-02-24 14:00:53 -0500

    [Fix]: The 7-print_chessboard.c file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,13 @@
 void print_chessboard(char (*a)[8])
 {
 	unsigned int c, ch;
-
-	for (c = 0; c < 8; c++)
+	
+	for (c = 0 ; c < 8 ; c++)
 	{
-		for (ch = 0; ch < 8; ch++)
+		for (ch = 0 ; ch < 8 ; ch++)
 		{
-		_putchar(a[c][ch]);
+			_putchar(a[c][ch]);
 		}
+	_putchar ('\n');
 	}
-	_putchar('/n');
 }
