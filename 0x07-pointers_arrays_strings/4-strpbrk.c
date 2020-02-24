b96cb818e6ba9d2165cc05d93007a3f652a0293f
Manga08 <60020101+Manga08@users.noreply.github.com>
2020-02-24 13:31:44 -0500

    [Fix]: The 4-strpbrk.c file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,16 +7,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    unsigned int c, ch;
+	unsigned int c, ch;
 
-    for (c = 0; s[c]; c++)
-    {
-        for (ch = 0; accept [ch]; ch++)
-        {
-            if (s[c] == accept [ch] )
-            return(s + c);
-        }
-    if (s == c)
-        return(s + c);
-    }
+	for (c = 0; s[c]; c++)
+	{
+		for (ch = 0; accept [ch]; ch++)
+		{
+			if (accept[ch] == s[c])
+				return(s + c);
+		}
+		if (accept[ch] == s[c])
+			return(s + c);
+	}
+	return (0);
 }
