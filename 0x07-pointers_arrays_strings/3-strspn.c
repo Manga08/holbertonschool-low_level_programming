9bcb5d8be92691966339323f01df22deb54a7735
Manga08 <60020101+Manga08@users.noreply.github.com>
2020-02-24 13:24:40 -0500

    [Fix]: The 3-strspn.c file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 /**
- * _memcpy - check the code for Holberton School students.
- *@src: memory area
- *@dest: another memory area
- *@n: byte form memory aarea of src
+ * _strspn - check the code for Holberton School students.
+ *@s: memory area
+ *@accept: another memory area
  *Return: always 0
  */
- unsigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int c, ch, counter = 0;
+	unsigned int c, ch, counter = 0;
 
-    for (c = 0; s[c]; c++)
-    {
-        for (ch = 0; accept [ch]; ch++)
-        {
-            if (s[c] == accept [ch] )
-            counter++;
-        }
-        if (counter < c)
-        break;
-    }
-    return (counter);
+	for (c = 0; s[c]; c++)
+	{
+		for (ch = 0; accept [ch]; ch++)
+		{
+			if (s[c] == accept [ch])
+				counter++;
+		}
+		if (counter < c)
+			break;
+	}
+	return (counter);
 }
