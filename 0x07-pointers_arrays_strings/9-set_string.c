e596d738ccbbbb9a7e0aba3c81e18021e6a51bc2
Manga08 <60020101+Manga08@users.noreply.github.com>
2020-02-24 14:18:52 -0500

    [Featured]: Upload the 9-set_string.c file

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - check the code for Holberton School students.
+ *@s: memory area
+ *@to: another memory area
+ *Return: always 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
