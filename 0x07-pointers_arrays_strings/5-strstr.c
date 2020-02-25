4c2cd3114e8d881a75015f7c7a10a34bcd884fa2
Manga08 <bijuu5445@gamail.com>
2020-02-24 19:50:01 -0500

    [Featured]: The 5-strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strstr - check the code for Holberton School students.
+ *@haystack: memory area
+ *@needle: another memory area
+ *Return: always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *c, *ch;
+
+	while (*haystack != '\0')
+	{
+		c = haystack;
+		ch = needle;
+
+		while (*ch == *haystack && *ch != '\0'
+		       && *haystack != '\0')
+		{
+			haystack++;
+			ch++;
+		}
+		if (*ch == '\0')
+			return (c);
+		haystack = c + 1;
+	}
+	return (NULL);
+}
