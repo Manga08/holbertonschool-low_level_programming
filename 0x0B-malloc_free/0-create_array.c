1689eebd77934c169060d2fa029a68e52e8d4abe
Manga08 <bijuu5445@gmail.com>
2020-03-03 10:57:17 -0500

    [Update]: The 0x0B-malloc_free files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * create_array - check the code for Holberton School students.
  *@size:The integer
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * _strdup - check the code for Holberton School students.
  *@str:The integer
@@ -6,4 +8,21 @@
  */
 char *_strdup(char *str)
 {
+	int c, ch;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (c = 0; *(str + c) != '\0'; c++)
+	{
+	}
+	s = malloc(c + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (ch = 0; ch < c; ch++)
+		*(s + ch) = *(str + ch);
+	return (s);
 }
