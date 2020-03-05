5c4466fa12230b588db867e299b42b226722e379
Manga08 <bijuu5445@gamail.com>
2020-03-04 23:51:32 -0500

    [Update]: The 0x0B-malloc_free directories files

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - check the code for Holberton School students.
+ *@str: the variable
+ *Return: dest
+ */
+char **strtow(char *str)
+	
