724a214414909bd71406c4315380a3f633c7f820
Manga08 <bijuu5445@gmail.com>
2020-03-09 09:20:58 -0500

    [Update]: The 0x0D-preprocessor files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students
+ *Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
