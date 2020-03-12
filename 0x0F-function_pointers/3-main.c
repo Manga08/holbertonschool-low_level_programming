b9564ddf9a986c73ea1bac6d3df3499273cc1ee1
Manga08 <bijuu5445@gamail.com>
2020-03-11 23:21:40 -0500

    [Update]: The 0x0F-function_pointers files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -10,8 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1 /*= atoi(argv[1])*/, n2 /*= atoi(argv[3])*/, respuesta;
-	char operator, *signo;
+	char *signo;
+	char operador;
+	int n1, n2, respuesta;
 	int (*p)(int, int);
 
 	if (argc != 4)
@@ -19,16 +19,16 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	operator = argv[2][0];
-	if ((operator != '+' && operator != '-'
-	     && operator != '*' && operator != '/') || argv[2][1] != '\0')
+	operador = argv[2][0];
+	if ((operador != '*' && operador != '+' && operador != '-'
+	     && operador != '/'  && operador != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
-	if ((operator == '/' || operator == '%')&& n2 == 0)
+	if ((operador == '/' || operador == '%') && n2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -36,7 +36,6 @@ int main(int argc, char *argv[])
 	signo = argv[2];
 	p = get_op_func(signo);
 	respuesta = (*p)(n1, n2);
-
 	printf("%d\n", respuesta);
 	return (0);
 }
