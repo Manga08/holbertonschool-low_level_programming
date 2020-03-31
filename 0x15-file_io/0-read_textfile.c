dc1376fa172dd28d81efc50e396c8c0294faecc0
Manga08 <bijuu5445@gmail.com>
2020-03-31 15:10:25 -0500

    [Featured]: Upload the 0x15-file_io directory with the zero task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * read_textfile - check the code for Holberton School students.
+ *@filename: the variable
+ *@letters: the letters
+ *Return: the counter
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fd;
+	int fd_read, fd_write, fd_close;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (free(buffer), -1);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	fd_read = read(fd, buffer, letters);
+	if (fd_read == -1)
+		return (0);
+	fd_write = write(STDOUT_FILENO, buffer, letters);
+	if (fd_write == -1)
+		return (0);
+	fd_close = close(fd);
+	if (fd_close == -1)
+		return (0);
+	free(buffer);
+	return (fd_read);
+}
