1487eefe0162b76846cf41d3898199c3cfde94bd
Manga08 <bijuu5445@gmail.com>
2020-05-03 14:45:13 -0500

    [Fix]: The 7 task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,40 +8,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *curr = *h;
-	dlistint_t *new;
-	unsigned int i;
+	dlistint_t *location = *h, *new_node;
+	unsigned int ct;
 
 	if (!h)
 		return (NULL);
 	if (*h == NULL && idx != 0)
 		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
+	new_node->n = n;
 	if (idx == 0)
 	{
 		if (*h)
-			curr->prev = new;
-		*h = new;
-		new->prev = NULL;
-		new->next = curr;
-		return (new);
+			location->prev = new_node;
+		*h = new_node;
+		new_node->prev = NULL;
+		new_node->next = location;
+		return (new_node);
 	}
-	for (i = 1; i < idx; i++)
+	for (ct = 1; ct < idx; ct++)
 	{
-		curr = curr->next;
-		if (curr == NULL)
+		location = location->next;
+		if (location == NULL)
 		{
-			free(new);
+			free(new_node);
 			return (NULL);
 		}
 	}
-	new->prev = curr;
-	new->next = curr->next;
-	if (curr->next)
-		curr->next->prev = new;
-	curr->next = new;
-	return (new);
+	new_node->prev = location;
+	new_node->next = location->next;
+	if (location->next)
+		location->next->prev = new_node;
+	location->next = new_node;
+	return (new_node);
 }
