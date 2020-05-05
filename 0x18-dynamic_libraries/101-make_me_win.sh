62e533581b3f9c7c32e19300b8006c864a34b1de
Manga08 <bijuu5445@gmail.com>
2020-05-04 22:17:42 -0500

    [Fix]: The advanced task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./gm 9 8 10 24 75 9
+wget -P /tmp https://github.com/Manga08/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so
