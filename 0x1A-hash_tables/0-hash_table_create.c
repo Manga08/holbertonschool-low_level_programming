bf7d4aedc821321c32e6559723129e9787554894
Manga08 <bijuu5445@gmail.com>
2020-05-28 16:10:28 -0500

    [Featured]: The firts task and the readme file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ * Return: Pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t;
+
+	hash_t = malloc(sizeof(hash_table_t));
+	if (!hash_t)
+		return (NULL);
+
+	hash_t->array = calloc(size, sizeof(hash_node_t *));
+	if (!hash_t)
+	{
+		free(hash_t);
+		return (NULL);
+	}
+	hash_t->size = size;
+	return (hash_t);
+}
