e9871bca4cf9c45c7edaeedb13d1b09c8eb14384
Manga08 <bijuu5445@gmail.com>
2020-08-27 23:25:29 -0500

    [Featured]: The Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,14 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-void print_array(int *array, unsigned int start, unsigned int end);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int binary_search_recursive(int *array, int low, int high, int value);
-int advanced_binary(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
